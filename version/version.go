dea4fe9138769139467b71e87e15c0c61c97ac70
Alex <alex@yanchenko.me>
2021-06-20 22:29:44 +0200

    chabge version representation

--- version/version.go
+++ version/version.go
@@ -23,7 +23,7 @@
 package version
 
 // VERSION is current Gifo version
-const VERSION = "1.0.7"
+const VERSION = "1.0.9.#CI_PID-#CI_HASH (#CI_DATE)"
 
 // VERSIONDB is current System DB API VERSION
 const VERSIONDB = "1.0"
